26c7cd8119ce70f130825d05b69cc08506965f8e
iudah <olaolujude@gmail.com>
2024-01-16 07:11:55 +0100

    Move Vec4 Implementation to Vec4.cpp
    This commit refactors the code by moving the implementation of the Vec4 class from the main.cpp file to a dedicated source file, Vec4.cpp. The class definition remains in the Vec4.hpp file. This separation improves code organization and adheres to the best practices of modular programming.
    
    Changes:
    - Moved the implementation of the Vec4 class functions to Vec4.cpp.
    - Updated the main.cpp file to include only the necessary header files.
    - No changes to the functionality of the Vec4 class, ensuring a smooth transition.

---
+++ Vec4.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+
+#include "Vec4.hpp"
+
+namespace LRay {
+
+Vec4::Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+
+Vec4 Vec4::add(const Vec4 &b) const {
+  return LRay::Vec4(x + b.x, y + b.y, z + b.z, w + b.w);
+}
+
+Vec4 Vec4::minus(const Vec4 &b) const {
+  return Vec4(x - b.x, y - b.y, z - b.z, w - b.w);
+}
+
+Vec4 Vec4::unit() const { return (*this) / mag(); }
+
+float Vec4::angle(const Vec4 &b) const {
+  float result = std::acos(dot(b) / (mag() * b.mag()));
+  return result;
+}
+
+} // namespace LRay
