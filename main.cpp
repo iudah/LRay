8b52368e73f76941f8869bf32ac4cf07aff47b2f
iudah <olaolujude@gmail.com>
2024-01-16 06:55:00 +0100

    Refactor main.cpp: Include "Vec4.hpp" and organize class definitions
    This commit enhances the structure of main.cpp by replacing the <cmath> include with "Vec4.hpp" to include the necessary class declarations. Additionally, it organizes the class definitions, removing redundant class declarations for Vec4 and improving the inheritance structure for Vec3 and Ray.
    
    Changes:
    - Replaced <cmath> include with "Vec4.hpp" to ensure proper inclusion of class declarations.
    - Organized class definitions for better readability and maintainability.
    - Removed redundant Vec4 class declarations and implementations in main.cpp.
    - Improved the inheritance structure for the Ray class, now inheriting from Vec3.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <cmath>
+#include "Vec4.hpp"
 
 namespace LRay {
 // A class declaration representing 3d object
@@ -17,72 +17,6 @@ class Vec3;
 class Ray;
 }; // namespace LRay
 
-// Vec4
-class LRay::Vec4 {
-protected:
-  float x;
-  float y;
-  float z;
-  float w;
-
-public:
-  // create a 4d vector i.e. A =(x,y,z,w)
-  Vec4(float x = 0, float y = 0, float z = 0, float w = 0);
-
-  // create a copy of a 4d vector i.e. B = A
-  Vec4(Vec4 &vec4);
-  
-  // Add two vectors i.e. C = A + B => C_x = A_x + B_x; C_y = A_y + B_y ...
-  LRay::Vec4 add(LRay::Vec4 &b);
-  
-  // Minus two vectors i.e. C = A - B => C_x = A_x - B_x; C_y = A_y - B_y ...
-  LRay::Vec4 minus(LRay::Vec4 &b);
-  
-  // Multiply two vectors i.e. C = A * B => C_x = A_x * B_x; C_y = A_y * B_y ...
-  LRay::Vec4 times(LRay::Vec4 &b);
-  
-  // Multiply vector by scalar s i.e. C = A * s
-  
-  // => C_x = A_x * s; C_y = A_y * s...
-  LRay::Vec4 times(float s);
-  
-  // Dot product of two vectors
-  float dot(LRay::Vec4 &b);
-  
-  // Angle btwn two vectors
-  float angle(LRay::Vec4 &b){
-    float result = acos(this->dot(b)/(this->mag() * b.mag()));
-    return result;
-  };
-  
-  // magnitude of vector
-  float mag();
-  
-  // compute the unit vector
-  LRay::Vec4 unit();
-  
-  // Divide vector by scalar s i.e. C = A / s
-  // => C_x = A_x / s; C_y = A_y / s...
-  LRay::Vec4 divide(float s);
-  
-  // override division sign to divide by scalar
-  LRay::Vec4 operator/(float s);
-};
-
-LRay::Vec4::Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
-
-LRay::Vec4 LRay::Vec4::add(LRay::Vec4 &b) {
-  return LRay::Vec4(this->x + b.x, this->y + b.y, this->z + b.z, this->w + b.w);
-}
-LRay::Vec4 LRay:: Vec4::minus(LRay::Vec4 &b){
-  float x = this -> x - b.x;
-  float y = this -> y - b.y;
-  float z = this -> z - b.z;
-  float w = this -> w - b.w;
-  return LRay::Vec4(x,y,z,w);
-}
-LRay::Vec4 LRay::Vec4::unit() { return (*this) / this->mag(); }
-
 class LRay::Vec3 : public LRay::Vec4 {
   Vec3(float x = 0, float y = 0, float z = 0);
   Vec3(Vec3 &vec3);
@@ -90,7 +24,7 @@ class LRay::Vec3 : public LRay::Vec4 {
 
 class LRay::_3DObject {};
 
-class LRay::Ray {};
+class LRay::Ray : public LRay::Vec3 {};
 
 class LRay::Camera {
 private:
